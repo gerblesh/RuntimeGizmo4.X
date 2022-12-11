3232e9ccb68b24f1d7be047f0ec077abe09f5714
gerblesh <101901964+gerblesh@users.noreply.github.com>
2022-12-10 19:22:14 -0800

    almost done goddammit, upgraded to beta 8

--- src/Script/GizmoManager.gd
+++ src/Script/GizmoManager.gd
@@ -2,11 +2,22 @@ extends Node3D
 
 @onready var camera : Camera3D = get_viewport().get_camera_3d()
 
+@export var selected_node : Node3D
+
+var gizmo_container_idx : int = 0
+var packed_gizmos : Array[PackedScene] = [
+	preload("res://src/Scene/TranslateGizmo.tscn"),
+	preload("res://src/Scene/ScaleGizmo.tscn"),
+]
+
+
 
 var selected_gizmo : Node3D
 var last_hover : Node3D
 const ray_length = 10000
 
+func _ready():
+	select_object(selected_node,packed_gizmos[gizmo_container_idx])
 
 func cast_ray_from_camera(event: InputEvent,camera : Camera3D) -> Dictionary:
 	var start_coordinate : Vector3 = camera.project_ray_origin(event.position)
@@ -22,6 +33,16 @@ func cast_ray_from_camera(event: InputEvent,camera : Camera3D) -> Dictionary:
 # Hand the mouse input of clicking and hovering over an object
 ################################################################################
 func _unhandled_input(event : InputEvent) -> void:
+	# temp gizmo changing
+	if Input.is_key_pressed(KEY_W):
+		select_object(selected_node,packed_gizmos[0])
+		get_viewport().set_input_as_handled()
+		return
+	if Input.is_key_pressed(KEY_E):
+		select_object(selected_node,packed_gizmos[1])
+		get_viewport().set_input_as_handled()
+		return
+	
 	# If the left mouse button is clicked.
 	if event is InputEventMouseButton and event.button_index == 1:
 		get_viewport().set_input_as_handled()
@@ -75,8 +96,13 @@ func _unhandled_input(event : InputEvent) -> void:
 			last_hover = null
 
 func select_object(node : Node3D, gizmo_scene : PackedScene):
-	for child in get_children():
-		child.queue_free()
+	clear_runtime_gizmo()
 	var gizmo : Node3D = gizmo_scene.instantiate()
+	gizmo.global_position = node.global_position
 	gizmo.node = node
 	add_child(gizmo)
+
+func clear_runtime_gizmo():
+	selected_gizmo = null
+	for child in get_children():
+		child.queue_free()
--- src/Script/YPlaneDragScale.gd
+++ src/Script/YPlaneDragScale.gd
@@ -20,18 +20,17 @@ func select(_camera : Camera3D, event : InputEvent) -> void:
 	plane = [plane_range.x, 0.0, plane_range.y, k]
 
 	start_offset = get_offset_coordinates(event,camera,plane)
-
 	start_scale = node.scale
-# find the nearest global axis based on the basis of the object
+	
+	# find the nearest global axis based on the basis of the object
 	var axis_index : int = axis.max_axis_index()
 	for i in 3:
 		var vector = abs(node.global_transform.basis[i])
 		if vector.max_axis_index() == axis_index:
 			var new_axis : Vector3 = Vector3.ZERO
 			new_axis[i] = 1
-			
 			scale_axis = new_axis
 
 
 func apply_transform(_position : Vector3):
-	node.scale = start_scale + (_position[axis.max_axis_index()] * scale_axis * factor)
+	node.scale = abs(start_scale + (_position[axis.max_axis_index()] * scale_axis * factor))
